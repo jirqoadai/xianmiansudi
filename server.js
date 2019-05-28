22148e5a6eba05d6d16005d413e27851f23bae97
jirqoadai <jirqoadai@qq.com>
2019-05-28 08:04:35 +0800

    init

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path    = require('path');
+const http    = require('http');
+
+const server  = express();
+server.set('port', process.env.PORT || 10001);
+
+// route
+server.use('/fonts', express.static(path.join(__dirname, './fonts')));
+server.use('/css', express.static(path.join(__dirname, './css')));
+server.use('/js', express.static(path.join(__dirname, './js')));
+server.use('/img', express.static(path.join(__dirname, './img')));
+server.use('/favicon.ico', express.static(path.join(__dirname, './favicon.ico')));
+server.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+
+http.createServer(server).listen(server.get('port'), (err) => {
+    if (err) {
+        throw err
+    }
+    console.log(`Application worker ${process.pid} started... at port ${server.get('port')}`);
+});
